73b6c984e09dd0ec1b504230d03ee986053bae49
Katia Bezrukova <bezrukova.catherina@gmail.com>
2018-06-23 16:45:38 -0400

    included images to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,30 @@ export default class Home extends React.Component {
     render() {
         return (
             <body>
-                HOME PAGE!!
+            <div className = "container" id = "mainCards">
+            <div className = "row">
+              <div className="offset-lg-1 col-lg-5 col-md-5">
+
+            <div className="card mainCard">
+              <img className="card-img-top" src="images/study.jpg" alt="Card image cap" />
+              <div className="card-body">
+
+                <a href="#" className="btn btn-primary login">Learn more</a>
+              </div>
+            </div>
+            </div>
+            <div className="col-lg-5 col-md-5">
+            <div className="card mainCard">
+              <img className="card-img-top" src="images/career.jpg" alt="Card image cap" />
+              <div className="card-body">
+
+                <a href="#" className="btn btn-primary login">Learn more</a>
+              </div>
+            </div>
+            </div>
+            </div>
+            </div>
+
             </body>
         );
     }
