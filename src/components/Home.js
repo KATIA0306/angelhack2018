e6dcca4ae777667e23fb7ec3e2bbd296df5e03ab
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2018-06-23 17:14:14 -0400

    Linked tiles to appropriate pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends React.Component {
     render() {
         return (
             <body>
             <div className = "container" id = "mainCards">
-            <div className = "row">
-              <div className="offset-lg-1 col-lg-5 col-md-5">
-
-            <div className="card mainCard">
-              <img className="card-img-top" src="images/study.jpg" alt="Card image cap" />
-              <div className="card-body">
-
-                <a href="#" className="btn btn-primary login">Learn more</a>
+              <div className = "row">
+                <div className="offset-lg-1 col-lg-5 col-md-5">
+                  <div className="card mainCard">
+                    <Link to='/schools'>
+                      <img className="card-img-top" src="images/study.jpg" alt="Card image cap" />
+                    </Link>
+                    <div className="card-body">
+                      <Link to='/schools'>
+                        <a href="#" className="btn btn-primary login">Learn more</a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-5 col-md-5">
+                  <div className="card mainCard">
+                    <Link to='/employment'>
+                      <img className="card-img-top" src="images/career.jpg" alt="Card image cap" />
+                    </Link>
+                    <div className="card-body">
+                      <Link to='/employment'>
+                        <a href="#" className="btn btn-primary login">Learn more</a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            </div>
-            <div className="col-lg-5 col-md-5">
-            <div className="card mainCard">
-              <img className="card-img-top" src="images/career.jpg" alt="Card image cap" />
-              <div className="card-body">
-
-                <a href="#" className="btn btn-primary login">Learn more</a>
-              </div>
-            </div>
-            </div>
-            </div>
-            </div>
-
             </body>
         );
     }
