9cf7cea3cc290a921161824ccb0b63491946945e
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2018-06-23 14:36:28 -0400

    Separated App into two new components Body and Header

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Body extends React.Component {
+    render() {
+        return (
+            <body>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </body>
+        );
+    }
+}
+
+export default Body;
