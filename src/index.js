1740a1cbe203dd550707137df2d7764399e8c9fd
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2018-06-23 14:11:16 -0400

    Installed (and basically integrated ) React Router package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
+
+
 registerServiceWorker();
